529735904455567faf1980967fd33599789e938d
Fabien Lamarque <flamarque@octo.com>
2018-09-18 16:16:47 +0200

    added a number of distinct values

--- src/main/java/DevRandomSimulator.java
+++ src/main/java/DevRandomSimulator.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DevRandomSimulator {
 
     private String seed;
@@ -15,12 +18,22 @@ public class DevRandomSimulator {
         }
     }
 
-    public void runAFewTimes() {
+    public void runAFewTimes(int numberOfOutputs) {
+        List<Character> results = new ArrayList<>();
         CharRandomizer timeCharRandomizer = new TimeCharRandomizer();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < numberOfOutputs; i++) {
             char randomChar = timeCharRandomizer.getRandomChar(seed.hashCode());
             System.out.print(randomChar);
             seed = String.valueOf(randomChar);
+            results.add(randomChar);
         }
+
+        System.out.println("Results of all generated Numbers:");
+        System.out.println(numberOfDifferentValues(results, numberOfOutputs));
+
+    }
+
+    private String numberOfDifferentValues(List<Character> results, int numberOfOutputs) {
+        return String.format("\t Distinct values : %s out of %s", results.stream().distinct().count(), numberOfOutputs);
     }
 }
--- src/test/java/DevRandomSimulatorTest.java
+++ src/test/java/DevRandomSimulatorTest.java
@@ -1,11 +1,9 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class DevRandomSimulatorTest {
 
     @Test
     public void runAFewTimes() {
-        new DevRandomSimulator("lkqjsdf").runAFewTimes();
+        new DevRandomSimulator("lkqjsdf").runAFewTimes(1000);
     }
 }
